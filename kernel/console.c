87f1c7c67dfa5ad310f992d767c717646621f81e
bookeydoo <waleedbookey@gmail.com>
2025-11-29 19:06:03 +0200

    added syscall kbdint

--- kernel/console.c
+++ kernel/console.c
@@ -30,6 +30,8 @@
 // called by printf(), and to echo input characters,
 // but not from write().
 //
+int kbd_intr_count=0;
+
 void
 consputc(int c)
 {
@@ -43,7 +45,7 @@ consputc(int c)
 
 struct {
   struct spinlock lock;
-  
+
   // input
 #define INPUT_BUF_SIZE 128
   char buf[INPUT_BUF_SIZE];
@@ -136,6 +138,7 @@ void
 consoleintr(int c)
 {
   acquire(&cons.lock);
+  ++kbd_intr_count;
 
   switch(c){
   case C('P'):  // Print process list.
@@ -174,7 +177,7 @@ consoleintr(int c)
     }
     break;
   }
-  
+
   release(&cons.lock);
 }
 
